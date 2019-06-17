3dfc3ebd53062b992a6bfc01c7e08cce0c6d8425
chencheng <dainel_txy@163.com>
2019-06-17 16:37:50 +0800

    change babelTsReact cache true

--- pipe/babelTsReact.js
+++ pipe/babelTsReact.js
@@ -15,7 +15,7 @@ module.exports = (config) => {
                     exclude: /(node_modules|bower_modules)/,
                     test: /\.tsx?$/,
                     options: {
-                        cacheDirectory: false,
+                        cacheDirectory: true,
                         "env": {
                             // 生产环境下去掉prop-types
                             "production": {
